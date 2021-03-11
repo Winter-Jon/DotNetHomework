f175e2acec47524774b993fabdf1bad5b483fd4d
文韬 蒋 <57853258+Winter-Jon@users.noreply.github.com>
2021-03-11 22:05:59 +0800

    第二次作业

---
+++ homework2/problem1/Program1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace problem1
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入一个整数：");
+            int a =Convert.ToInt32(Console.ReadLine());
+
+            int[] fac = Factor(a);
+
+            Console.WriteLine("该数的素因数有：");
+            Console.WriteLine(fac[0]);
+            for (int i = 1; i < fac.Length; i++)
+            {
+                for(int j= 0;j<=i;j++)
+                {
+                    if(fac[i]%fac[j]==0)
+                        break;
+                    else if(j>=i-1)
+                        Console.WriteLine(fac[i]);
+                }
+            }
+
+
+
+        }
+
+        static int[] Factor(int a)
+        {
+            int[] fac;
+            fac = new int[10000];
+            fac[1] = 0;
+            int factorNum = 0;
+
+            for (int i = 2; i <= a / 2; i++)
+            {
+                if (a % i == 0)
+                {
+                    fac[factorNum] = i;
+                    factorNum++;
+                }
+            }
+
+            int[] facCopy = new int[factorNum];
+
+            for(int i=0;i<factorNum;i++)
+            {
+                facCopy[i] = fac[i];
+            }
+            return facCopy;
+        }
+
+
+    }
+}
---
+++ homework2/problem3/Program3.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace problem3
+{
+    class Program3
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[99];
+
+            for(int i=0;i<99;i++)
+                a[i] = i+2;
+
+            int pt = 0;
+            do
+            {
+                a=Remove(a, a[pt]);
+                pt++;
+            }
+            while (a.Length!= Remove(a, a[pt]).Length);
+
+            for (int i = 0; i < a.Length; i++)
+                Console.WriteLine(a[i]); 
+
+        }
+
+        static int[] Remove(int[] a,int n)
+        {
+            int len=a.Length;
+            for(int i=0;i<a.Length;i++)
+            {
+                if(a[i]%n==0&&a[i]!=n)
+                {
+                    a[i] = 0;
+                    len--;
+                }
+            }
+
+            int[] b = new int[len];
+            int num = 0;
+            for(int i=0;i<a.Length;i++)
+            {
+                if(a[i]!=0)
+                {
+                    b[num] = a[i];
+                    num++;
+                }
+            }
+            return b;
+        }
+
+
+
+    }
+}
