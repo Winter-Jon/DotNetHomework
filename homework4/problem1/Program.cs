79710e0ef826a52aefe6251e64eebee34665ebbe
文韬 蒋 <57853258+Winter-Jon@users.noreply.github.com>
2021-03-20 01:40:32 +0800

    ……

---
+++ homework4/problem1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace problem1
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            head = tail = null;
+        }
+
+        public void Append(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if(tail==null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            Node<T> p = head;
+            while(p!=null)
+            {
+                action(p.Data);
+                p = p.Next;
+            }
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> a = new GenericList<int>();
+
+            a.Append(0);
+            a.Append(5);
+            a.Append(3);
+            a.Append(2);
+            a.Append(9);
+            a.Append(8);
+
+            a.ForEach(i =>{ Console.WriteLine(i); });
+
+            int max= -2147483648;
+            a.ForEach(i => { max = i > max ? i : max; });
+            Console.WriteLine(max);
+
+            int min = 2147483647;
+            a.ForEach(i => { min = i < min ? i : min; });
+            Console.WriteLine(min);
+
+            int sum = 0;
+            a.ForEach(i => { sum += i; });
+            Console.WriteLine(sum);
+
+        }
+    }
+}
---
+++ homework4/problem2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Timers;
+
+namespace problem2
+{
+    class Program
+    {
+
+        public class Clock
+        {
+            private int alarmTime;
+            private int curTime;
+
+            public delegate void TickHandle();
+            public delegate void AlarmHandle();
+            public event TickHandle TickEvent;
+            public event AlarmHandle AlarmEvent;
+
+            public void TickLog()
+            {
+                Console.WriteLine("Tick!");
+            }
+
+            public void AlarmLog()
+            {
+                Console.WriteLine("Alarm!");
+            }
+
+            public Clock()
+            {
+                TickEvent = TickLog;
+                AlarmEvent = AlarmLog;
+                Tick();
+                Alarm();
+            }
+
+            public void setAlarmTime(int t)
+            {
+                alarmTime = t;
+            }
+
+            private void Tick()
+            {
+                TickEvent();
+                curTime += 1;
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            private void Alarm()
+            {
+                if (curTime == alarmTime)
+                    AlarmEvent();
+            }
+
+            public void Start()
+            {
+                while(true)
+                {
+                    Tick();
+                    Alarm();
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Clock clock = new Clock();
+            clock.setAlarmTime(15);
+
+            clock.Start();
+        }
+    }
+}
